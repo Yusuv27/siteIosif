5692452d7e68bc252bcb06d071ed15e7cbff8bf1
Yusuv27 <133457274+Yusuv27@users.noreply.github.com>
2025-04-06 19:15:11 +0300

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidebar").style.left = "0"; // Показываем сайдбар
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.left = "-250px"; // Скрываем сайдбар
+}
